42591c0a7cbbe42622076fc0d3ec76bc0c6936ec
boy1089 <38555647+boy1089@users.noreply.github.com>
2018-07-15 15:07:13 +0900

    Add files via upload

---
+++ open cv/thresholding/adaptive.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('dog.jpg')
+img = img[:, :, 0]
+img = cv2.GaussianBlur(img, (5, 5), 0)
+
+
+blockSize = 301
+C = 1
+th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,blockSize,C)
+#th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+#            cv2.THRESH_BINARY,blockSize,C)
+th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,blockSize,C)
+
+plt.subplot(211)
+plt.imshow(th2, cmap = 'gray')
+plt.subplot(212)
+plt.imshow(th3, cmap = 'gray')
+
+#plt.imshow(img, cmap = 'gray')
+#plt.plot(th2[100])
---
+++ open cv/thresholding/otsu.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('dog.jpg')
+img1 = img[:, :, 0]
+img = cv2.copyMakeBorder(img1, 100, 100, 100, 100, cv2.BORDER_REPLICATE)
+#img = cv2.GaussianBlur(img, (5, 5), 0)
+blockSize = 51
+C = 1
+img_ret = img.copy()
+img_th = img.copy()
+for ix, iy in np.ndindex(img.shape):
+    img_seg = img[ix - int(blockSize/2):ix+int(blockSize/2), iy - int(blockSize/2):iy+int(blockSize/2)]
+    ret, th = cv2.threshold(img_seg, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    img_ret[ix, iy] = ret
+    if img_th[ix, iy] >= ret:
+        img_th[ix, iy] = 1
+    else : 
+        img_th[ix, iy] = 0
+
+imgList = [img_ret, img_th, img1, img]
+imgTitle = ['threshold', 'result', 'original', 'padding']
+for j in range(4):
+    plt.subplot('22%s'%j)
+    plt.imshow(imgList[j], cmap = 'gray')
+    plt.title(imgTitle[j])
